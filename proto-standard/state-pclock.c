3925ed686ed4f0ee3de53b8d8ccbff2bf337f36a
C.Prados <c.prados@gsi.de>
2014-10-20 12:16:47 +0200

    pdelay/pclock: add pclock state to the standard protocol
    pclock is a clock involved in the calculation
    of the link delay using Peer Delay Mechanism.
    This calculation is independent of whether a clock
    is a master/slave. There is delay requester, pclock_req,
    and, and delay responder, pclock_resp.
    The Grandmaster clock is the only one in charge of sending Sync messages
    and complete the synchronization. In case of BC, one
    port e.g. port 1 is a pclock connected to the upstream
    Grandmaster Clock, and the rest of ports, downstream, Grandmaster
    Clocks. In a TC, all the ports are just pclocks, who calculate
    the link delay and forward the sync and follow up msg till the slaves.
    
    Signed-off-by: C.Prados <c.prados@gsi.de>

---
+++ proto-standard/state-pclock.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2014 GSI (www.gsi.de)
+ * Author: Cesar Prados
+ * Based on PTPd project v. 2.1.0 (see AUTHORS for details)
+ * Released according to the GNU LGPL, version 2.1 or any later version.
+ */
+
+#include <ppsi/ppsi.h>
+#include "common-fun.h"
+
+int pp_pclock(struct pp_instance *ppi, unsigned char *pkt, int plen)
+{
+
+	return 0;
+}

6a78aa1bbb74c5f119d0ed4a66db51a9c3a45e61
Alessandro Rubini <rubini@gnudd.com>
2015-07-24 19:26:12 +0200

    Revert "wr-servo: trivial: move a line so it's clearer"
    This reverts commit 1d5a461d9ccc03094017ae4fbde50d564ccfd9af.
    
    actually, the error count must be zeroed every time, not only after
    printing: we want to print if the error persists for 5 iterations, not
    every 5th time.
    
    My bad....
    
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

--- proto-ext-whiterabbit/wr-servo.c
+++ proto-ext-whiterabbit/wr-servo.c
@@ -242,12 +242,10 @@ int wr_servo_update(struct pp_instance *ppi)
 	if(!s->t1.correct || !s->t2.correct ||
 	   !s->t3.correct || !s->t4.correct) {
 		errcount++;
-		if (errcount > 5) { /* a 2-3 in a row are expected */
+		if (errcount > 5) /* a 2-3 in a row are expected */
 			pp_error("%s: TimestampsIncorrect: %d %d %d %d\n",
 				 __func__, s->t1.correct, s->t2.correct,
 				 s->t3.correct, s->t4.correct);
-			errcount = 0;
-		}
 		return 0;
 	}
 
@@ -256,6 +254,8 @@ int wr_servo_update(struct pp_instance *ppi)
 
 	/* shmem lock */
 	wrs_shm_write(ppsi_head, WRS_SHM_WRITE_BEGIN);
+
+	errcount = 0;
 
 	s->update_count++;
 

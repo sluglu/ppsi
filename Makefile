sluglu/ppsi

commit b88106b30eda771f16a422f2c0131f4d530a412c

trivial: Makefile: fix for make v4
Signed-off-by: C.Prados <c.prados@gsi.de>

Makefile
@@ -102,7 +102,7 @@ silentoldconfig:
 	@mkdir -p include/config
 	$(MAKE) -f Makefile.kconfig $@
 
-scripts_basic config %config:
+%config:
 	$(MAKE) -f Makefile.kconfig $@
 
 defconfig:

commit e2df2d6556610589e88d279f9f313464836a4e2e

Makefile: fix "make config"
Fix command make config. The error was probably introduced by commit:
b88106b3 trivial: Makefile: fix for make v4

Signed-off-by: Adam Wujek <adam.wujek@cern.ch>

Makefile
@@ -106,6 +106,9 @@ silentoldconfig:
 	@mkdir -p include/config
 	$(MAKE) -f Makefile.kconfig $@
 
+scripts_basic config:
+	$(MAKE) -f Makefile.kconfig $@
+
 %config:
 	$(MAKE) -f Makefile.kconfig $@
 
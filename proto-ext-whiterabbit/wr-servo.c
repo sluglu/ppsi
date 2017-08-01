sluglu/ppsi

commit 35eef6790031977ef186b2a2dd0579eebc908fdb

time: introduce a new unified data structure
Commit only build for arch-unix (and no tools either)
This simplifies a lot the calculations.

  - all times are now living in a single structure
  - all fields are signed and 64-bit long
  - the 2038 bug is over (we still had 32-bit seconds)
  - no overflow in math or unexplained "unsigned" appearing

Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

proto-ext-whiterabbit/wr-servo.c
@@ -234,7 +234,7 @@ int wr_servo_got_sync(struct pp_instance *ppi, TimeInternal *t1,
 	return 0;
 }
 
-int wr_servo_got_delay(struct pp_instance *ppi, Integer32 cf)
+int wr_servo_got_delay(struct pp_instance *ppi)
 {
 	struct wr_servo_state *s =
 			&((struct wr_data *)ppi->ext_data)->servo_state;
@@ -244,15 +244,13 @@ int wr_servo_got_delay(struct pp_instance *ppi, Integer32 cf)
 	s->t3 = ppi->t3;
 	/*  s->t3.phase = 0; */
 	s->t4 = ppi->t4;
-	s->t4.correct = 1; /* clock->delay_req_receive_time.correct; */
-	s->t4.phase = (int64_t) cf * 1000LL / 65536LL;
+	/* FIXME: verify that cField is already merged */
 
 	if (CONFIG_HAS_P2P && ppi->mech == PP_P2P_MECH) {
 		s->t5 = ppi->t5;
 		s->t5.correct = 1;
 		s->t5.phase = 0;
 		s->t6 = ppi->t6;
-		s->t6.phase = (int64_t) ppi->t6_cf * 1000LL / 65536LL;
 
 		wr_p2p_delay(ppi, s);
 	}

fsm.c
@@ -16,7 +16,7 @@ unsigned long pp_global_d_flags; /* This is the only "global" file in ppsi */
 static void pp_fsm_printf(struct pp_instance *ppi, char *fmt, ...)
 {
 	va_list args;
-	TimeInternal t;
+	struct pp_time t;
 	unsigned long oflags = pp_global_d_flags;
 
 	if (!pp_diag_allow(ppi, fsm, 1))
@@ -28,7 +28,7 @@ static void pp_fsm_printf(struct pp_instance *ppi, char *fmt, ...)
 	pp_global_d_flags = oflags;
 
 	pp_printf("diag-fsm-1-%s: %09d.%03d: ", ppi->port_name,
-		  (int)t.seconds, (int)t.nanoseconds / 1000000);
+		  (int)t.secs, (int)((t.scaled_nsecs >> 16)) / 1000000);
 	va_start(args, fmt);
 	pp_vprintf(fmt, args);
 	va_end(args);
@@ -203,10 +203,10 @@ int pp_state_machine(struct pp_instance *ppi, uint8_t *packet, int plen)
 	if (plen) {
 		msgtype = packet[0] & 0xf;
 		pp_diag(ppi, frames, 1,
-			"RECV %02d bytes at %d.%09d (type %x, %s)\n", plen,
-			(int)ppi->last_rcv_time.seconds,
-			(int)ppi->last_rcv_time.nanoseconds, msgtype,
-			pp_msgtype_info[msgtype].name);
+			"RECV %02d bytes at %9d.%09d (type %x, %s)\n", plen,
+			(int)ppi->last_rcv_time.secs,
+			(int)(ppi->last_rcv_time.scaled_nsecs >> 16),
+			msgtype, pp_msgtype_info[msgtype].name);
 	}
 
 	/*

include/ppsi/ieee1588_types.h
@@ -11,6 +11,7 @@
 #define __PPSI_IEEE_1588_TYPES_H__
 
 #include <stdint.h>
+#include <ppsi/pp-time.h>
 
 /* See F.2, pag.223 */
 #define PP_ETHERTYPE	0x88f7
@@ -60,30 +61,6 @@ typedef struct Timestamp { /* page 13 (33) -- no typedef expected */
 	UInteger48	secondsField;
 	UInteger32	nanosecondsField;
 } Timestamp;
-
-typedef struct TimeInternal {
-	Integer32	seconds;
-	Integer32	nanoseconds;
-	/* White Rabbit extension begin */
-	Integer32	phase;		/* This is the set point */
-	int 		correct;	/* 0 or 1 */
-#if 0
-	/*
-	 * The following two fields may be used for diagnostics, but
-	 * they cost space. So remove them but keep the code around just
-	 * in case it is useful again (they are only set, never read)
-	 */
-	int32_t raw_phase;
-	int32_t raw_nsec;
-#endif
-	int32_t raw_ahead;	/* raw_ahead is used during calibration */
-	/* White Rabbit extension end */
-} TimeInternal;
-
-static inline void clear_TimeInternal(struct TimeInternal *t)
-{
-	memset(t, 0, sizeof(*t));
-}
 
 typedef struct ClockIdentity { /* page 13 (33) */
 	Octet id[8];
@@ -136,7 +113,7 @@ typedef struct MsgHeader {
 	UInteger16	messageLength;
 	UInteger8	domainNumber;
 	Octet		flagField[2];
-	Integer64	correctionfield;
+	struct pp_time	cField;;
 	PortIdentity	sourcePortIdentity;
 	UInteger16	sequenceId;
 	UInteger8	controlField;
@@ -158,40 +135,40 @@ typedef struct MsgAnnounce {
 
 /* Sync Message (table 26, page 129) */
 typedef struct MsgSync {
-	Timestamp originTimestamp;
+	struct pp_time originTimestamp;
 } MsgSync;
 
 /* DelayReq Message (table 26, page 129) */
 typedef struct MsgDelayReq {
-	Timestamp	originTimestamp;
+	struct pp_time	originTimestamp;
 } MsgDelayReq;
 
 /* DelayResp Message (table 27, page 130) */
 typedef struct MsgFollowUp {
-	Timestamp	preciseOriginTimestamp;
+	struct pp_time	preciseOriginTimestamp;
 } MsgFollowUp;
 
 
 /* DelayResp Message (table 28, page 130) */
 typedef struct MsgDelayResp {
-	Timestamp	receiveTimestamp;
+	struct pp_time	receiveTimestamp;
 	PortIdentity	requestingPortIdentity;
 } MsgDelayResp;
 
 /* PdelayReq Message (table 29, page 131) */
 typedef struct MsgPDelayReq {
-	Timestamp	originTimestamp;
+	struct pp_time	originTimestamp;
 } MsgPDelayReq;
 
 /* PdelayResp Message (table 30, page 131) */
 typedef struct MsgPDelayResp {
-	Timestamp	requestReceiptTimestamp;
+	struct pp_time	requestReceiptTimestamp;
 	PortIdentity	requestingPortIdentity;
 } MsgPDelayResp;
 
 /* PdelayRespFollowUp Message (table 31, page 132) */
 typedef struct MsgPDelayRespFollowUp {
-	Timestamp	responseOriginTimestamp;
+	struct pp_time	responseOriginTimestamp;
 	PortIdentity	requestingPortIdentity;
 } MsgPDelayRespFollowUp;
 
@@ -229,8 +206,8 @@ typedef struct DSDefault {		/* page 65 */
 typedef struct DSCurrent {		/* page 67 */
 	/* Dynamic */
 	UInteger16	stepsRemoved;
-	TimeInternal	offsetFromMaster;
-	TimeInternal	meanPathDelay; /* oneWayDelay */
+	struct pp_time	offsetFromMaster;
+	struct pp_time	meanPathDelay; /* oneWayDelay */
 	/* White Rabbit extension begin */
 	UInteger16	primarySlavePortNumber;
 	/* White Rabbit extension end */
@@ -256,7 +233,6 @@ typedef struct DSPort {			/* page 72 */
 	/* Dynamic */
 	/* Enumeration8	portState; -- not used */
 	Integer8	logMinDelayReqInterval; /* -- same as pdelay one */
-	/* TimeInternal	peerMeanPathDelay; -- not used */
 	/* Configurable */
 	Integer8	logAnnounceInterval;
 	UInteger8	announceReceiptTimeout;

include/ppsi/pp-instance.h
@@ -82,14 +82,14 @@ struct pp_frgn_master {
  * where increasing the stiffness (s) lowers the cutoff and increases the delay.
  */
 struct pp_avg_fltr {
-	Integer32 m; /* magnitude */
-	Integer32 y;
-	Integer32 s_exp;
+	int64_t m; /* magnitude */
+	int64_t y;
+	int64_t s_exp;
 };
 
 struct pp_servo {
-	TimeInternal m_to_s_dly;
-	TimeInternal s_to_m_dly;
+	struct pp_time m_to_s_dly;
+	struct pp_time s_to_m_dly;
 	long long obs_drift;
 	struct pp_avg_fltr mpd_fltr;
 };
@@ -149,10 +149,10 @@ struct pp_instance {
 	uint16_t peer_vid;	/* Our peer's VID (for PROTO_VLAN) */
 
 	/* Times, for the various offset computations */
-	TimeInternal t1, t2, t3, t4, t5, t6;		/* *the* stamps */
+	struct pp_time t1, t2, t3, t4, t5, t6;		/* *the* stamps */
 	Integer32 t4_cf, t6_cf;				/* peer delay */
-	TimeInternal cField;				/* transp. clocks */
-	TimeInternal last_rcv_time, last_snt_time;	/* two temporaries */
+	struct pp_time cField;				/* transp. clocks */
+	struct pp_time last_rcv_time, last_snt_time;	/* two temporaries */
 
 	/* Page 85: each port shall maintain an implementation-specific
 	 * foreignMasterDS data set for the purposes of qualifying Announce

include/ppsi/pp-time.h
@@ -1,0 +1,24 @@
+#ifndef __PPSI_PP_TIME_H__
+#define __PPSI_PP_TIME_H__
+
+/* Everything internally uses this time format, *signed* */
+struct pp_time {
+	int64_t		secs;
+	int64_t		scaled_nsecs;
+};
+
+/* The "correct" bit is hidden in the hight bits */
+static inline int is_incorrect(const struct pp_time *t)
+{
+	return (((t->secs >> 56) & 0xc0) == 0x80);
+}
+static inline void mark_incorrect(struct pp_time *t)
+{
+	t->secs |= (0x8fLL << 56);
+}
+static inline void clear_time(struct pp_time *t)
+{
+	memset(t, 0, sizeof(*t));
+}
+
+#endif /* __PPSI_PP_TIME_H__ */

include/ppsi/ppsi.h
@@ -22,6 +22,7 @@
 
 #include <arch/arch.h> /* ntohs and so on -- and wr-api.h for wr archs */
 
+
 /* At this point in time, we need ARRAY_SIZE to conditionally build vlan code */
 #undef ARRAY_SIZE
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
@@ -154,8 +155,8 @@ struct pp_ext_hooks {
 	void (*s1)(struct pp_instance *ppi, MsgHeader *hdr, MsgAnnounce *ann);
 	int (*execute_slave)(struct pp_instance *ppi);
 	int (*handle_announce)(struct pp_instance *ppi);
-	int (*handle_followup)(struct pp_instance *ppi, TimeInternal *orig,
-			       TimeInternal *correction_field);
+	int (*handle_followup)(struct pp_instance *ppi, struct pp_time *orig,
+			       struct pp_time *correction_field);
 	int (*handle_preq) (struct pp_instance * ppi);
 	int (*handle_presp) (struct pp_instance * ppi);
 	int (*pack_announce)(struct pp_instance *ppi);
@@ -173,7 +174,7 @@ struct pp_network_operations {
 	int (*init)(struct pp_instance *ppi);
 	int (*exit)(struct pp_instance *ppi);
 	int (*recv)(struct pp_instance *ppi, void *pkt, int len,
-		    TimeInternal *t);
+		    struct pp_time *t);
 	int (*send)(struct pp_instance *ppi, void *pkt, int len, int msgtype);
 	int (*check_packet)(struct pp_globals *ppg, int delay_ms);
 };
@@ -192,8 +193,8 @@ extern struct pp_network_operations unix_net_ops;
  * If "set" receives a NULL time value, it should update the TAI offset.
  */
 struct pp_time_operations {
-	int (*get)(struct pp_instance *ppi, TimeInternal *t);
-	int (*set)(struct pp_instance *ppi, TimeInternal *t);
+	int (*get)(struct pp_instance *ppi, struct pp_time *t);
+	int (*set)(struct pp_instance *ppi, const struct pp_time *t);
 	/* freq_ppb is parts per billion */
 	int (*adjust)(struct pp_instance *ppi, long offset_ns, long freq_ppb);
 	int (*adjust_offset)(struct pp_instance *ppi, long offset_ns);
@@ -380,19 +381,16 @@ extern void *msg_copy_header(MsgHeader *dest, MsgHeader *src); /* REMOVE ME!! */
 extern int msg_issue_announce(struct pp_instance *ppi);
 extern int msg_issue_sync_followup(struct pp_instance *ppi);
 extern int msg_issue_request(struct pp_instance *ppi);
-extern int msg_issue_delay_resp(struct pp_instance *ppi, TimeInternal *time);
+extern int msg_issue_delay_resp(struct pp_instance *ppi,
+				struct pp_time *time);
 extern int msg_issue_pdelay_resp_followup(struct pp_instance *ppi,
-					  TimeInternal * time);
-extern int msg_issue_pdelay_resp(struct pp_instance *ppi, TimeInternal * time);
+					  struct pp_time *time);
+extern int msg_issue_pdelay_resp(struct pp_instance *ppi, struct pp_time *time);
 
-/* Functions for timestamp handling (internal to protocol format conversion*/
-/* FIXME: add prefix in function name? */
-extern void cField_to_TimeInternal(TimeInternal *internal, Integer64 bigint);
-extern int from_TimeInternal(TimeInternal *internal, Timestamp *external);
-extern int to_TimeInternal(TimeInternal *internal, Timestamp *external);
-extern void add_TimeInternal(TimeInternal *r, TimeInternal *x, TimeInternal *y);
-extern void sub_TimeInternal(TimeInternal *r, TimeInternal *x, TimeInternal *y);
-extern void div2_TimeInternal(TimeInternal *r);
+/* Functions for time math */
+extern void pp_time_add(struct pp_time *t1, struct pp_time *t2);
+extern void pp_time_sub(struct pp_time *t1, struct pp_time *t2);
+extern void pp_time_div2(struct pp_time *t);
 
 /*
  * The state machine itself is an array of these structures.

proto-ext-whiterabbit/hooks.c
@@ -257,8 +257,8 @@ static __attribute__((used)) int wr_handle_presp(struct pp_instance *ppi)
 		return 0;
 	}
 
-	ppi->t4_cf = hdr->correctionfield.lsb;
-	wr_servo_got_delay(ppi, ppi->t4_cf);
+	/* FIXME: verify that last-received cField is already accounted for */
+	wr_servo_got_delay(ppi);
 	return 0;
 }
 

proto-ext-whiterabbit/wr-api.h
@@ -133,8 +133,8 @@ enum {
 int wr_servo_init(struct pp_instance *ppi);
 void wr_servo_reset(struct pp_instance *ppi);
 void wr_servo_enable_tracking(int enable);
-int wr_servo_got_sync(struct pp_instance *ppi, TimeInternal *t1,
-		      TimeInternal *t2);
+int wr_servo_got_sync(struct pp_instance *ppi, struct pp_time *t1,
+		      struct pp_time *t2);
 int wr_servo_got_delay(struct pp_instance *ppi, Integer32 cf);
 int wr_servo_update(struct pp_instance *ppi);
 
@@ -156,12 +156,12 @@ struct wr_servo_state {
 	int32_t clock_period_ps;
 
 	/* These fields are used by servo code, across iterations */
-	TimeInternal t1, t2, t3, t4, t5, t6;
+	struct pp_time t1, t2, t3, t4, t5, t6;
 	int64_t delta_ms_prev;
 	int missed_iters;
 
 	/* Following fields are for monitoring/diagnostics (use w/ shmem) */
-	TimeInternal mu;
+	struct pp_time mu;
 	int64_t picos_mu;
 	int32_t cur_setpoint;
 	int64_t delta_ms;
@@ -176,14 +176,14 @@ struct wr_servo_state {
 	uint32_t n_err_state;
 	uint32_t n_err_offset;
 	uint32_t n_err_delta_rtt;
-	TimeInternal update_time;
+	struct pp_time update_time;
 };
 
 int wr_p2p_delay(struct pp_instance *ppi, struct wr_servo_state *s);
 int wr_e2e_offset(struct pp_instance *ppi,
-		  struct wr_servo_state *s, TimeInternal *ts_offset_hw);
+		  struct wr_servo_state *s, struct pp_time *ts_offset_hw);
 int wr_p2p_offset(struct pp_instance *ppi,
-		  struct wr_servo_state *s, TimeInternal *ts_offset_hw);
+		  struct wr_servo_state *s, struct pp_time *ts_offset_hw);
 
 
 /* All data used as extension ppsi-wr must be put here */

commit aff58d8379d30464c944f795a08dc03bf9eb53f2

[BUG: 1627] proto-ext-whiterabbit: fix pps for links longer than ~13km
For links longer than ~13km, a value (uint32_t) returned from a function
__div64_32 shifted by 16 bits overflowed, introducing PPS offset.

Bug introduced in the commit:
35eef679 time: introduce a new unified data structure

Signed-off-by: Adam Wujek <adam.wujek@cern.ch>

proto-ext-whiterabbit/wr-servo.c
@@ -80,7 +80,7 @@ static void picos_to_ts(int64_t picos, struct pp_time *ts)
 	phase = __div64_32(&nsec, 1000);
 	sec = nsec;
 
-	ts->scaled_nsecs = __div64_32(&sec, PP_NSEC_PER_SEC) << 16;
+	ts->scaled_nsecs = ((int64_t)__div64_32(&sec, PP_NSEC_PER_SEC)) << 16;
 	ts->scaled_nsecs += (phase << 16) / 1000;
 	ts->scaled_nsecs *= sign;
 	ts->secs = sec * sign;
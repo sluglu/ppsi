319c9147589766b8e06156f4d9d7f5cd3214d742
Sven Meier <sven.meier@nettimelogic.com>
2017-08-24 09:29:30 +0200

    hal: ported wrs hal to ppsi as well

--- proto-ext-whiterabbit/wr-api.h
+++ proto-ext-whiterabbit/wr-api.h
@@ -11,7 +11,7 @@
 
 /* Please increment WRS_PPSI_SHMEM_VERSION if you change any exported data
  * structure */
-#define WRS_PPSI_SHMEM_VERSION 29 /* added get_servo_state to time ops */
+#define WRS_PPSI_SHMEM_VERSION 30 /* added HAL_PORT_STATE_RESET to hal */
 
 /* Don't include the Following when this file is included in assembler. */
 #ifndef __ASSEMBLY__

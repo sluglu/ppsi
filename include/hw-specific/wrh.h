36553dfe2539aa60a871038a44f763b4884a8000
Adam Wujek <adam.wujek@creotech.pl>
2020-11-03 16:36:10 +0100

    include/hw-specific: allow to include from assembler
    Include only WRS_PPSI_SHMEM_VERSION, when this file is included from assembler,
    which is the case for WRPC.
    
    Signed-off-by: Adam Wujek <adam.wujek@creotech.pl>

--- include/hw-specific/wrh.h
+++ include/hw-specific/wrh.h
@@ -9,12 +9,13 @@
 #ifndef __WRH_H__
 #define __WRH_H__
 
-#include <stdint.h>
-
-#include <ppsi/lib.h>
-
 /* Please increment WRS_PPSI_SHMEM_VERSION if you change any exported data structure */
 #define WRS_PPSI_SHMEM_VERSION 33
+
+/* Don't include the Following when this file is included in assembler. */
+#ifndef __ASSEMBLY__
+#include <stdint.h>
+#include <ppsi/lib.h>
 
 /* White Rabbit softpll status values */
 #define WRH_SPLL_ERROR		  -1
@@ -152,5 +153,7 @@ extern int     wrh_servo_got_resp(struct pp_instance *ppi);
 extern int     wrh_servo_got_presp(struct pp_instance *ppi);
 extern int     wrh_update_correction_values(struct pp_instance *ppi);
 
+
+#endif /* __ASSEMBLY__ */
 #endif /* __WRH_H__ */
 

154be34a3ee0ca3d77b4dce47566d407c48b1187
Tristan Gingold <tristan.gingold@cern.ch>
2023-03-28 11:55:15 +0200

    wr-state-machine.c: constify

--- proto-ext-whiterabbit/wr-state-machine.c
+++ proto-ext-whiterabbit/wr-state-machine.c
@@ -10,50 +10,38 @@
 #include "wr-constants.h"
 #include <math.h>
 
+typedef int (*wr_state_action_t)(struct pp_instance *ppi, void *buf, int len, int new_state);
+
+
 typedef struct {
 	char *name;
-	int (*action)(struct pp_instance *ppi, void *buf, int len, int new_state);
+
 }wr_state_machine_t;
+
+static const wr_state_action_t wr_state_actions[] ={
+	[WRS_IDLE] = wr_idle,
+	[WRS_PRESENT] = wr_present,
+	[WRS_M_LOCK] = wr_m_lock,
+	[WRS_S_LOCK] = wr_s_lock,
+	[WRS_LOCKED] = wr_locked,
+	[WRS_CALIBRATION] = wr_calibration,
+	[WRS_CALIBRATED] = wr_calibrated,
+	[WRS_RESP_CALIB_REQ] = wr_resp_calib_req,
+	[WRS_WR_LINK_ON] = wr_link_on,
+};
 
 #define MAX_STATE_ACTIONS (sizeof(wr_state_actions)/sizeof(wr_state_machine_t))
 
-static wr_state_machine_t wr_state_actions[] ={
-		[WRS_IDLE]{
-			.name="wr-idle",
-			.action=wr_idle,
-		},
-		[WRS_PRESENT]{
-			.name="wr-present",
-			.action=wr_present,
-		},
-		[WRS_M_LOCK]{
-			.name="wr-m-lock",
-			.action=wr_m_lock,
-		},
-		[WRS_S_LOCK]{
-			.name="wr-s-lock",
-			.action=wr_s_lock,
-		},
-		[WRS_LOCKED]{
-			.name="wr-locked",
-			.action=wr_locked,
-		},
-		[WRS_CALIBRATION]{
-			.name="wr-calibration",
-			.action=wr_calibration,
-		},
-		[WRS_CALIBRATED]{
-			.name="wr-calibrated",
-			.action=wr_calibrated,
-		},
-		[WRS_RESP_CALIB_REQ]{
-			.name="wr-resp-calib-req",
-			.action=wr_resp_calib_req,
-		},
-		[WRS_WR_LINK_ON]{
-			.name="wr-link-on",
-			.action=wr_link_on,
-		},
+static const char * const wr_state_names[] ={
+	[WRS_IDLE] = "wr-idle",
+	[WRS_PRESENT] = "wr-present",
+	[WRS_M_LOCK] = "wr-m-lock",
+	[WRS_S_LOCK] = "wr-s-lock",
+	[WRS_LOCKED] = "wr-locked",
+	[WRS_CALIBRATION] = "wr-calibration",
+	[WRS_CALIBRATED] = "wr-calibrated",
+	[WRS_RESP_CALIB_REQ] = "wr-resp-calib-req",
+	[WRS_WR_LINK_ON] = "wr-link-on",
 };
 
 /*
@@ -61,27 +49,30 @@ static wr_state_machine_t wr_state_actions[] ={
  * It is used to send signaling messages.
  * It returns the ext-specific timeout value
  */
-int wr_run_state_machine(struct pp_instance *ppi, void *buf, int len) {
+int wr_run_state_machine(struct pp_instance *ppi, void *buf, int len)
+{
 	struct wr_dsport * wrp=WR_DSPOR(ppi);
 	Boolean newState;
 	int delay;
 
-	if ( wrp->next_state>=MAX_STATE_ACTIONS ||  ppi->state==PPS_INITIALIZING ||  !(ppi->extState==PP_EXSTATE_ACTIVE)) {
+	if (wrp->next_state>=MAX_STATE_ACTIONS
+	    || ppi->state==PPS_INITIALIZING
+	    || !(ppi->extState==PP_EXSTATE_ACTIVE)) {
 		wrp->next_state=WRS_IDLE; // Force to IDLE state
 	}
 
 	/*
 	 * Evaluation of events common to all states
 	 */
-	newState=wrp->state!=wrp->next_state;
+	newState = wrp->state != wrp->next_state;
 	if ( newState ) {
 		pp_diag(ppi, ext, 2, "WR state: LEAVE=%s, ENTER=%s\n",
-				wr_state_actions[wrp->state].name,
-				wr_state_actions[wrp->next_state].name);
+				wr_state_names[wrp->state],
+				wr_state_names[wrp->next_state]);
 		wrp->state=wrp->next_state;
 	}
 
-	delay=(*wr_state_actions[wrp->state].action) (ppi, buf,len, newState);
+	delay=(*wr_state_actions[wrp->state]) (ppi, buf,len, newState);
 
 	return delay;
 }
